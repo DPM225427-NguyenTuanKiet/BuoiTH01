087606f10e6873265f93b933e286430c31d8737c
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2025-09-12 00:19:22 +0700

    Add files via upload

---
+++ DPM225427_NguyenTuanKiet_Pattern_06_Adapter/Pattern/Adapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    public class Adapter:Target
+    {
+        private Adaptee adaptee = new Adaptee();
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+}
---
+++ DPM225427_NguyenTuanKiet_Pattern_06_Adapter/Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+            Target target = new Adapter();
+            target.Request();
+            Console.ReadKey();
+        }
+    }
+}
