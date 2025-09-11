bf5f8d16855e2881e0ecad0c9354b3fcc41a0bf8
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2025-09-12 00:23:41 +0700

    Add files via upload

---
+++ DPM225427_NguyenTuanKiet_Real_06_Adapter/Real/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Compound unknown = new Compound();
+            unknown.Display();
+            Compound water = new RichCompound("Water");
+            water.Display();
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+            Console.ReadKey();
+        }
+    }
+}
