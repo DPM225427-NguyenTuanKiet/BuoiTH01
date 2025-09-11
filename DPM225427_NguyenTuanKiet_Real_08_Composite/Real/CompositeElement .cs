fbf22f3093d464804888f1d1d655724268170600
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2025-09-12 00:24:38 +0700

    Add files via upload

---
+++ DPM225427_NguyenTuanKiet_Real_08_Composite/Real/CompositeElement .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real
+{
+    public class CompositeElement:DrawingElement
+    {
+        List<DrawingElement> elements = new List<DrawingElement>();
+        public CompositeElement(string name)
+            : base(name)
+        {
+        }
+        public override void Add(DrawingElement d)
+        {
+            elements.Add(d);
+        }
+        public override void Remove(DrawingElement d)
+        {
+            elements.Remove(d);
+        }
+        public override void Display(int indent)
+        {
+            Console.WriteLine(new String('-', indent) + "+ " + name);
+            foreach (DrawingElement d in elements)
+            {
+                d.Display(indent + 2);
+            }
+        }
+    }
+}
