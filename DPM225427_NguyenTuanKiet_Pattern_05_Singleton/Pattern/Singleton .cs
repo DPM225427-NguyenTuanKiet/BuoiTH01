6531ea054e7fa10f2d7224992e7c1c2b6650f1f5
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2025-09-12 00:18:54 +0700

    Add files via upload

---
+++ DPM225427_NguyenTuanKiet_Pattern_05_Singleton/Pattern/Singleton .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    public class Singleton
+    {
+        static Singleton instance;
+
+        protected Singleton()
+        {
+        }
+        public static Singleton Instance()
+        {
+
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
