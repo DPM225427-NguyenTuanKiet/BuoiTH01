33cc13bcbdaffd2462508696eb75d0a72e2c771d
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2025-09-12 00:16:06 +0700

    Add files via upload

---
+++ DPM225427_NguyenTuanKiet_Real_01_Abstract Factory/Real/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
+            Console.ReadKey();
+        }
+    }
+}
