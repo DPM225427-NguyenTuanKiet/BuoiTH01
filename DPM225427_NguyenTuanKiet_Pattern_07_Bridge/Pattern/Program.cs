04129d2244ae4ec15727e93b620f3d5863cff940
DPM225427-NguyenTuanKiet <kiet_dpm225427@student.agu.edu.vn>
2025-09-12 00:19:55 +0700

    Add files via upload

---
+++ DPM225427_NguyenTuanKiet_Pattern_07_Bridge/Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+            Console.ReadKey();
+        }
+    }
+}
